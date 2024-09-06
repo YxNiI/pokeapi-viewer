c85ce233d9236a014a338fe3d0a0b2317a1e7ad5
YxNiI <107792920+YxNiI@users.noreply.github.com>
2024-09-06 18:17:33 +0200

    Implemented a fundamental solution for displaying some poke-info as a paragraph.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log('Happy developing ✨')
+async function searchPokeInfo(name)
+{
+    let result = "pokemon-search-error";
+
+    try
+    {
+        result = await new Promise((resolve, reject) =>
+                                   {
+                                       const getRequest = new XMLHttpRequest();
+                                       getRequest.open("GET", ("https://pokeapi.co/api/v2/pokemon/" + name));
+                                       getRequest.send();
+                                       getRequest.onload = function()
+                                       {
+                                           if ((getRequest.readyState === 4) && (getRequest.status === 200))
+                                           {
+                                               resolve(getRequest.responseText);
+                                           }
+                                           else
+                                           {
+                                               reject(
+                                                   `error-code: ${getRequest.status}, message: ${getRequest.statusText}`);
+                                           }
+                                       }
+                                   });
+    }
+    catch
+        (error)
+    {
+        console.error(error);
+    }
+
+    return result
+}
+
+document.getElementById("search-button").addEventListener("click", () =>
+{
+    const pokeInfo = searchPokeInfo(document.getElementById("search-bar").value);
+    const pokeInfoParagraph = document.createElement("p");
+    pokeInfo.then((pokeInfoValue) =>
+                  {
+                      pokeInfoParagraph.textContent = pokeInfoValue
+                  });
+    document.body.appendChild(pokeInfoParagraph);
+});
