5fcdc260a6a2b0fd54dcd3ad6c81ddfab13a7ae8
YxNiI <107792920+YxNiI@users.noreply.github.com>
2024-09-06 20:01:27 +0200

    Rudimentary implementation of getting pokemon-data in a formatted manner, as multiple p-elements.

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ async function searchPokeInfo(name)
                                    {
                                        const getRequest = new XMLHttpRequest();
                                        getRequest.open("GET", ("https://pokeapi.co/api/v2/pokemon/" + name));
+                                       getRequest.responseType = "json";
                                        getRequest.send();
                                        getRequest.onload = function()
                                        {
                                            if ((getRequest.readyState === 4) && (getRequest.status === 200))
                                            {
-                                               resolve(getRequest.responseText);
+                                               resolve(getRequest.response);
                                            }
                                            else
                                            {
@@ -34,11 +35,32 @@ async function searchPokeInfo(name)
 
 document.getElementById("search-button").addEventListener("click", () =>
 {
-    const pokeInfo = searchPokeInfo(document.getElementById("search-bar").value);
-    const pokeInfoParagraph = document.createElement("p");
-    pokeInfo.then((pokeInfoValue) =>
-                  {
-                      pokeInfoParagraph.textContent = pokeInfoValue
-                  });
-    document.body.appendChild(pokeInfoParagraph);
+    searchPokeInfo(document.getElementById("search-bar").value).then((pokeInfoValue) =>
+                                                                     {
+                                                                         const pokeInfoArr = [];
+                                                                         pokeInfoArr.push(
+                                                                             "Name: " + pokeInfoValue.name);
+                                                                         pokeInfoArr.push(
+                                                                             "Species: " + pokeInfoValue.species.name);
+                                                                         pokeInfoArr.push(
+                                                                             "Types: " + pokeInfoValue.types.map(
+                                                                                           pokemonType => pokemonType.type.name)
+                                                                                                      .join(", "));
+                                                                         pokeInfoArr.push(
+                                                                             "Size: " + pokeInfoValue.height);
+                                                                         pokeInfoArr.push(
+                                                                             "Abilities: "
+                                                                             + pokeInfoValue.abilities.map(
+                                                                                 pokemonAbility => pokemonAbility.ability.name)
+                                                                                            .join(", "));
+                                                                         pokeInfoArr.forEach(
+                                                                             foo =>
+                                                                             {
+                                                                                 const pokeInfoParagraph = document.createElement(
+                                                                                     "p");
+                                                                                 pokeInfoParagraph.textContent = foo;
+                                                                                 document.body.appendChild(
+                                                                                     pokeInfoParagraph);
+                                                                             });
+                                                                     });
 });
