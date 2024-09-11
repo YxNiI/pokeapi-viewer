2edf719215f484d5e2eeb66f8c42bd192c3f318b
Lyra Claus <l.claus@mce-bank.eu>
2024-09-11 09:47:32 +0200

    Reformatted the JavaScript-Code.

--- index.js
+++ index.js
@@ -5,26 +5,24 @@ async function searchPokeInfo(name)
     try
     {
         result = await new Promise((resolve, reject) =>
-                                   {
-                                       const getRequest = new XMLHttpRequest();
-                                       getRequest.open("GET", ("https://pokeapi.co/api/v2/pokemon/" + name));
-                                       getRequest.responseType = "json";
-                                       getRequest.send();
-                                       getRequest.onload = function()
-                                       {
-                                           if ((getRequest.readyState === 4) && (getRequest.status === 200))
-                                           {
-                                               resolve(getRequest.response);
-                                           }
-                                           else
-                                           {
-                                               reject(
-                                                   `error-code: ${getRequest.status}, message: ${getRequest.statusText}`);
-                                           }
-                                       }
-                                   });
-    }
-    catch
+        {
+            const getRequest = new XMLHttpRequest();
+            getRequest.open("GET", ("https://pokeapi.co/api/v2/pokemon/" + name));
+            getRequest.responseType = "json";
+            getRequest.send();
+            getRequest.onload = function ()
+            {
+                if ((getRequest.readyState === 4) && (getRequest.status === 200))
+                {
+                    resolve(getRequest.response);
+                } else
+                {
+                    reject(
+                        `error-code: ${getRequest.status}, message: ${getRequest.statusText}`);
+                }
+            }
+        });
+    } catch
         (error)
     {
         console.error(error);
@@ -36,37 +34,37 @@ async function searchPokeInfo(name)
 document.getElementById("search-button").addEventListener("click", () =>
 {
     searchPokeInfo(document.getElementById("search-bar").value).then((pokeInfo) =>
-                                                                     {
-                                                                         const sprite = document.createElement("img");
-                                                                         sprite.style.display = "block";
-                                                                         sprite.src =
-                                                                             pokeInfo.sprites.front_default;
-                                                                         document.body.appendChild(sprite);
+    {
+        const sprite = document.createElement("img");
+        sprite.style.display = "block";
+        sprite.src =
+            pokeInfo.sprites.front_default;
+        document.body.appendChild(sprite);
 
-                                                                         const pokeInfos = [];
-                                                                         pokeInfos.push(
-                                                                             "Name: " + pokeInfo.name);
-                                                                         pokeInfos.push(
-                                                                             "Species: " + pokeInfo.species.name);
-                                                                         pokeInfos.push(
-                                                                             "Types: " + pokeInfo.types.map(
-                                                                                           pokemonType => pokemonType.type.name)
-                                                                                                 .join(", "));
-                                                                         pokeInfos.push(
-                                                                             "Size: " + pokeInfo.height);
-                                                                         pokeInfos.push(
-                                                                             "Abilities: "
-                                                                             + pokeInfo.abilities.map(
-                                                                                 pokemonAbility => pokemonAbility.ability.name)
-                                                                                       .join(", "));
-                                                                         pokeInfos.forEach(
-                                                                             info =>
-                                                                             {
-                                                                                 const pokeInfoParagraph = document.createElement(
-                                                                                     "p");
-                                                                                 pokeInfoParagraph.textContent = info;
-                                                                                 document.body.appendChild(
-                                                                                     pokeInfoParagraph);
-                                                                             });
-                                                                     });
+        const pokeInfos = [];
+        pokeInfos.push(
+            "Name: " + pokeInfo.name);
+        pokeInfos.push(
+            "Species: " + pokeInfo.species.name);
+        pokeInfos.push(
+            "Types: " + pokeInfo.types.map(
+                pokemonType => pokemonType.type.name)
+                .join(", "));
+        pokeInfos.push(
+            "Size: " + pokeInfo.height);
+        pokeInfos.push(
+            "Abilities: "
+            + pokeInfo.abilities.map(
+                pokemonAbility => pokemonAbility.ability.name)
+                .join(", "));
+        pokeInfos.forEach(
+            info =>
+            {
+                const pokeInfoParagraph = document.createElement(
+                    "p");
+                pokeInfoParagraph.textContent = info;
+                document.body.appendChild(
+                    pokeInfoParagraph);
+            });
+    });
 });
