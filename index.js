f11fd4fe131285b505313da4471f08c58fc92689
Lyra Claus <l.claus@mce-bank.eu>
2024-09-13 10:32:59 +0200

    Some small code-structure-changes.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const maybePokeCards = sessionStorage.getItem("pokeInfos");
 const pokeInfos = (maybePokeCards !== null) ? JSON.parse(maybePokeCards) : [];
-
 searchPokeInfo("nihilego").then(createPokeCard);
 searchPokeInfo("celesteela").then(createPokeCard);
 searchPokeInfo("weezing").then(createPokeCard);
@@ -10,28 +9,22 @@ document.getElementById("search-button").addEventListener("click", () =>
     searchPokeInfo(document.getElementById("search-bar").value).then(createPokeCard);
 });
 
-async function searchPokeInfo(name)
+async function searchPokeInfo(pokeName)
 {
-    const pokeCardsFiltered = pokeInfos.filter(pokeCard =>
-    {
-        console.log(pokeCard.name + ' ' + name);
-        return pokeCard.name === name
-    });
+    const pokeInfosFiltered = pokeInfos.filter(pokeCard => pokeCard.name === pokeName);
     let result = "pokemon-search-error";
 
-    if (pokeCardsFiltered.length > 0)
+    if (pokeInfosFiltered.length > 0)
     {
-        console.log("blub")
-        result = pokeCardsFiltered[0].pokeInfo;
+        result = pokeInfosFiltered[0].pokeInfo;
     } else
     {
         try
         {
-            console.log("blub-blub");
             result = await new Promise((resolve, reject) =>
             {
                 const getRequest = new XMLHttpRequest();
-                getRequest.open("GET", ("https://pokeapi.co/api/v2/pokemon/" + name));
+                getRequest.open("GET", ("https://pokeapi.co/api/v2/pokemon/" + pokeName));
                 getRequest.responseType = "json";
                 getRequest.send();
                 getRequest.onload = function ()
@@ -42,13 +35,11 @@ async function searchPokeInfo(name)
 
                     } else
                     {
-                        reject(
-                            `error-code: ${getRequest.status}, message: ${getRequest.statusText}`);
+                        reject(`error-code: ${getRequest.status}, message: ${getRequest.statusText}`);
                     }
                 }
             });
-        } catch
-            (error)
+        } catch (error)
         {
             console.error(error);
         }
@@ -60,46 +51,45 @@ async function searchPokeInfo(name)
 function createPokeCard(pokeInfo)
 {
     const card = document.createElement("div");
-    card.classList.add("poke-card")
+    card.classList.add("poke-card");
 
     const sprite = document.createElement("img");
     sprite.classList.add("sprite");
     sprite.style.display = "block";
-    sprite.src =
-        pokeInfo.sprites.front_default;
+    sprite.src = pokeInfo.sprites.front_default;
     card.appendChild(sprite);
 
-    const info = [];
-    info.push(
+    const infos = [];
+    infos.push(
         "Name: " + pokeInfo.name.charAt(0).toUpperCase() + pokeInfo.species.name.slice(1));
     if (pokeInfo.name !== pokeInfo.species.name)
     {
-        info.push(
+        infos.push(
             "Species: " + pokeInfo.species.name.charAt(0).toUpperCase() + pokeInfo.species.name.slice(1));
     }
-    info.push(
+    infos.push(
         "Types: " + pokeInfo.types.map(pokemonType => pokemonType.type.name).join(", "));
-    info.push(
+    infos.push(
         "Size: " + pokeInfo.height);
-    info.push(
+    infos.push(
         "Abilities: " + pokeInfo.abilities.map(pokemonAbility => pokemonAbility.ability.name).join(", "));
     if (Array.isArray(pokeInfo.forms) && (pokeInfo.forms.length > 1))
     {
-        info.push("Forms: " + pokeInfo.forms.map(pokemonForm => pokemonForm.name).join(", "));
+        infos.push("Forms: " + pokeInfo.forms.map(pokemonForm => pokemonForm.name).join(", "));
     }
-    info.push("Stats: " + pokeInfo.stats.map(pokemonStat => pokemonStat.stat.name + ": \"" + pokemonStat.base_stat + "\"").join(", "));
-    info.push("Moves (last-three): " + pokeInfo.moves.slice((pokeInfo.moves.length - 3)).map(pokemonMove => pokemonMove.move.name).join(", "));
-    info.forEach(
-        info =>
-        {
-            const pokeInfoParagraph = document.createElement(
-                "p");
-            pokeInfoParagraph.textContent = info;
-            card.appendChild(
-                pokeInfoParagraph);
-        });
+    infos.push("Stats: " + pokeInfo.stats.map(pokemonStat => pokemonStat.stat.name + ": \"" + pokemonStat.base_stat + "\"").join(", "));
+    infos.push("Moves (last-three): " + pokeInfo.moves.slice((pokeInfo.moves.length - 3)).map(pokemonMove => pokemonMove.move.name).join(", "));
+    infos.forEach(info =>
+    {
+        const pokeInfoParagraph = document.createElement(
+            "p");
+        pokeInfoParagraph.textContent = info;
+        card.appendChild(
+            pokeInfoParagraph);
+    });
 
-    document.getElementById("card-viewer").prepend(card);
     pokeInfos.unshift({name: pokeInfo.name, pokeInfo: pokeInfo});
     sessionStorage.setItem("pokeInfos", JSON.stringify(pokeInfos));
+
+    document.getElementById("card-viewer").prepend(card);
 }
